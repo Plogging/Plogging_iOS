207a8f0fdf890114d8f0804086425a86d1de58dd
김혜리 <hyer1k@naver.com>
2021-01-06 00:18:15 +0900

    pod list update

--- Pods/Target Support Files/Pods-Plogging/Pods-Plogging-frameworks.sh
+++ Pods/Target Support Files/Pods-Plogging/Pods-Plogging-frameworks.sh
@@ -176,9 +176,27 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKAuth/KakaoSDKAuth.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKCommon/KakaoSDKCommon.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKLink/KakaoSDKLink.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKNavi/KakaoSDKNavi.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKStory/KakaoSDKStory.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKTalk/KakaoSDKTalk.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKTemplate/KakaoSDKTemplate.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKUser/KakaoSDKUser.framework"
+  install_framework "${PODS_ROOT}/naveridlogin-sdk-ios/NaverThirdPartyLogin.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKAuth/KakaoSDKAuth.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKCommon/KakaoSDKCommon.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKLink/KakaoSDKLink.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKNavi/KakaoSDKNavi.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKStory/KakaoSDKStory.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKTalk/KakaoSDKTalk.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKTemplate/KakaoSDKTemplate.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKUser/KakaoSDKUser.framework"
+  install_framework "${PODS_ROOT}/naveridlogin-sdk-ios/NaverThirdPartyLogin.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
