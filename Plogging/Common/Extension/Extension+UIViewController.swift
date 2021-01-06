f8fe691dc2bfe6a891ec2c95661d3edd3e7e42df
김혜리 <hyer1k@naver.com>
2021-01-06 20:27:04 +0900

    create common module

---
+++ Plogging/Common/Extension/Extension+UIViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Extension+UIViewController.swift
+//  Plogging
+//
+//  Created by 김혜리 on 2021/01/06.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showLoginViewController() {
+        let storyboard = UIStoryboard(name: "SNSLogin", bundle: nil)
+        if let loginViewController = storyboard.instantiateViewController(withIdentifier: "SNSLoginViewController") as? SNSLoginViewController {
+            loginViewController.modalPresentationStyle = .formSheet
+            loginViewController.isModalInPresentation = true
+            self.present(loginViewController, animated: true, completion: nil)
+        }
+    }
+}
