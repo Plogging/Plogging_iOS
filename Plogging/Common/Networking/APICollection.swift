06e7654077a9e44c6f305cdb5f2ee0e0bd448c1d
김혜리 <hyer1k@naver.com>
2021-01-11 21:31:15 +0900

    #7 create APICollection and baseURL

---
+++ Plogging/Common/Networking/APICollection.swift
@@ -0,0 +1,33 @@
+//
+//  APICollection.swift
+//  Plogging
+//
+//  Created by 김혜리 on 2021/01/11.
+//
+
+import Foundation
+import Alamofire
+
+struct APICollection {
+    static let sharedAPI = APICollection()
+
+    /// 세션키 발급
+    func requestSessionKey() {
+        
+    }
+    
+    /// 산책 이력 가져오기
+    func getWalkingRecord() {
+        
+    }
+    
+    /// 산책 이력 등록하기
+    func registerWalkingRecord() {
+        
+    }
+    
+    /// 산책정보 삭제
+    func deleteWalkingRecord() {
+        
+    }
+}
---
+++ Plogging/Common/Networking/BaseURL.swift
@@ -0,0 +1,20 @@
+//
+//  BaseURL.swift
+//  Plogging
+//
+//  Created by 김혜리 on 2021/01/11.
+//
+
+import Foundation
+
+struct BaseURL {
+    static let mainURL = "121.130.220.217:20000"
+}
+
+struct BasePath {
+    static let plogging = "/plogging"
+}
+
+struct BaseHeader {
+    static let userSessionKey = "userId"
+}
