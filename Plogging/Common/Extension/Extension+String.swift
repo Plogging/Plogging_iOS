d2923bffe9c20bb0837274f543af6b9aeec37431
김혜리 <hyer1k@naver.com>
2021-01-20 04:59:23 +0900

    #12 create showOnboardingViewController function extend string attribute

--- Plogging/Common/Extension/Extension+String.swift
+++ Plogging/Common/Extension/Extension+String.swift
@@ -5,4 +5,24 @@
 //  Created by 김혜리 on 2021/01/20.
 //
 
-import Foundation
+import UIKit
+
+extension NSMutableAttributedString {
+    func newLine(fontSize: CGFloat) -> NSMutableAttributedString {
+        let attrs: [NSAttributedString.Key: Any] = [.font: UIFont.boldSystemFont(ofSize: fontSize)]
+        self.append(NSMutableAttributedString(string: "\n", attributes: attrs))
+        return self
+    }
+    
+    func bold(_ text: String, fontSize: CGFloat) -> NSMutableAttributedString {
+        let attrs: [NSAttributedString.Key: Any] = [.font: UIFont.boldSystemFont(ofSize: fontSize)]
+        self.append(NSMutableAttributedString(string: text, attributes: attrs))
+        return self
+    }
+    
+    func normal(_ text: String, fontSize: CGFloat) -> NSMutableAttributedString {
+        let attrs: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: fontSize)]
+        self.append(NSMutableAttributedString(string: text, attributes: attrs))
+        return self
+    }
+}
--- Plogging/Common/Extension/Extension+UIViewController.swift
+++ Plogging/Common/Extension/Extension+UIViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
+enum Storyboard: String {
+    case SNSLogin
+    case Onboarding
+}
+
 extension UIViewController {
     func showLoginViewController() {
-        let storyboard = UIStoryboard(name: "SNSLogin", bundle: nil)
+        let storyboard = UIStoryboard(name: Storyboard.SNSLogin.rawValue, bundle: nil)
         if let loginViewController = storyboard.instantiateViewController(withIdentifier: "SNSLoginViewController") as? SNSLoginViewController {
             loginViewController.modalPresentationStyle = .formSheet
             loginViewController.isModalInPresentation = true
             self.present(loginViewController, animated: true, completion: nil)
+        }
+    }
+    
+    func showOnboardingViewController() {
+        let storyboard = UIStoryboard(name: Storyboard.Onboarding.rawValue, bundle: nil)
+        if let onboardingViewController = storyboard.instantiateViewController(withIdentifier: "OnboardingViewController") as? OnboardingViewController {
+            onboardingViewController.modalPresentationStyle = .fullScreen
+            self.present(onboardingViewController, animated: true, completion: nil)
         }
     }
 }
