5c45f12733c4bddefebe53117ad6d95dc148b20d
김혜리 <hyer1k@naver.com>
2021-01-22 02:33:33 +0900

    upload image

---
+++ Plogging/Plogging/WaitingScreen/WaitingScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WaitingScreenViewController.swift
+//  Plogging
+//
+//  Created by 김혜리 on 2021/01/22.
+//
+
+import UIKit
+
+class WaitingScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
