2c55df23a8722d2f4aa2955a03ef1922cec7c7c6
Kang_Taewoo <taewookang96@gmail.com>
2021-01-30 03:27:06 +0900

    fix summery stack

--- Plogging/Plogging/PloggingMain/PloggingRunningInfoViewController.swift
+++ Plogging/Plogging/PloggingMain/PloggingRunningInfoViewController.swift
@@ -7,42 +7,15 @@ import UIKit
 
 class PloggingRunningInfoViewController: UIViewController {
 
-    private var bodyView: PloggingRunningInfoView?
-
+    @IBOutlet weak var summeryStackView: UIStackView!
+    @IBOutlet weak var summeryDistance: SummeryItem!
+    @IBOutlet weak var summeryTime: SummeryItem!
+    @IBOutlet weak var summeryKcal: SummeryItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        bodyView = PloggingRunningInfoView()
-        guard let bodyView = bodyView else { return }
-
-        view.addSubview(bodyView)
-        
-        bodyView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            bodyView.topAnchor.constraint(equalTo: view.topAnchor),
-            bodyView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            bodyView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            bodyView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        ])
+        summeryDistance.setupView(unit: "킬로미터", value: "100")
+        summeryTime.setupView(unit: "분", value: "12:34")
+        summeryKcal.setupView(unit: "kcal", value: "399")
     }
 }
-
-#if canImport(SwiftUI) && DEBUG
-import SwiftUI
-@available(iOS 13.0, *)
-
-struct abcdefVCRepresentable: UIViewControllerRepresentable {
-    typealias UIViewControllerType = PloggingRunningInfoViewController
-    func makeUIViewController(context: Context) -> PloggingRunningInfoViewController {
-        PloggingRunningInfoViewController()
-    }
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-
-    }
-}
-
-struct abcdefPreviews: PreviewProvider {
-    static var previews: some View {
-        abcdefVCRepresentable()
-    }
-}
-#endif
