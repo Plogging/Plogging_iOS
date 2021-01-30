399c2347f77c147dfcfa4888da20230c121b2fca
김혜리 <hyer1k@naver.com>
2021-01-31 01:53:07 +0900

    check password validation

--- Plogging/Common/Extension/Extension+String.swift
+++ Plogging/Common/Extension/Extension+String.swift
@@ -28,9 +28,15 @@ extension NSMutableAttributedString {
 }
 
 extension String {
-    func isValidEmail(testStr: String) -> Bool {
+    func isValidEmail(email: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: testStr)
+        return emailTest.evaluate(with: email)
+    }
+    
+    func validpassword(password: String) -> Bool {
+        let passwordreg = ("(?=.*[A-Za-z])(?=.*[0-9]).{8,20}")
+        let passwordtesting = NSPredicate(format: "SELF MATCHES %@", passwordreg)
+        return passwordtesting.evaluate(with: password)
     }
 }
