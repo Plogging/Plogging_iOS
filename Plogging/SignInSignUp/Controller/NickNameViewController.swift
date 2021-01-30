c5b2f92c88e8fc3646b86240c77dd37b74fd1258
김혜리 <hyer1k@naver.com>
2021-01-31 03:14:31 +0900

    닉네임 validation check

--- Plogging/SignInSignUp/Controller/NickNameViewController.swift
+++ Plogging/SignInSignUp/Controller/NickNameViewController.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class NickNameViewController: UIViewController {
 
+    @IBOutlet weak var nickNameTextField: UITextField!
     @IBOutlet weak var nickNameInfoLabel: UILabel!
     @IBOutlet weak var confirmButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupTextField()
         setupUI()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func setupTextField() {
+        nickNameTextField.delegate = self
     }
     
     private func setupUI() {
@@ -24,6 +34,21 @@ class NickNameViewController: UIViewController {
     }
 
     @IBAction func clickConfirmButton(_ sender: UIButton) {
-        
+        guard let nickName = nickNameTextField.text else { return }
+        if nickName.count > 0 {
+            // reqeust API
+        }
+    }
+}
+
+extension NickNameViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        nickNameInfoLabel.isHidden = true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text?.count == 0 {
+            nickNameInfoLabel.isHidden = false
+        }
     }
 }
