91adf09ccb1da7f2115150dae35895b3fe697d85
jeon-soyeong <jsu3417@naver.com>
2021-02-02 13:44:30 +0900

    [#30] navagationbar 그라데이션 효과 적용

--- Plogging/My Page/Controller/MyPageViewController.swift
+++ Plogging/My Page/Controller/MyPageViewController.swift
@@ -8,22 +8,60 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
-
+    @IBOutlet weak var navigationBarView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    let itemSpacing: CGFloat = 8
+    var width = 0
+    var height = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpNavigationBarUI()
     }
     
+    func setUpNavigationBarUI() {
+        self.navigationController?.navigationBar.isHidden = true
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = navigationBarView.bounds
+        
+        let colors = [UIColor.tintGreen.cgColor, UIColor.lightGreenishBlue.cgColor]
+        gradientLayer.colors = colors
+         
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        gradientLayer.locations = [0.5]
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        navigationBarView.layer.addSublayer(gradientLayer)
+        
+        navigationBarView.clipsToBounds = true
+        navigationBarView.layer.cornerRadius = 37
+        navigationBarView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner, .layerMaxXMaxYCorner)
     }
-    */
 
+}
+
+// MARK: UICollectionViewDataSource
+//extension MyPageViewController: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//
+//    }
+//}
+
+// MARK: UICollectionViewDelegate
+extension MyPageViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+}
+// MARK: UICollectionViewDelegateFlowLayout
+extension MyPageViewController: UICollectionViewDelegateFlowLayout {
+    width = (collectionView.bounds.width - itemSpacing) / 2
+    height = width * 10/7 + itemSpacing
+    CGSize(width: width, height: height)
 }
