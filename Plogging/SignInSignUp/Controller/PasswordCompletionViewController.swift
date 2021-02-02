7c47c379296bfcd7f947f376e28da7a70fe22116
김혜리 <hyer1k@naver.com>
2021-02-03 04:12:10 +0900

    setup pw completion UI

---
+++ Plogging/SignInSignUp/Controller/PasswordCompletionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PasswordCompletionViewController.swift
+//  Plogging
+//
+//  Created by 김혜리 on 2021/02/03.
+//
+
+import UIKit
+
+class PasswordCompletionViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    private func setupUI() {
+        confirmButton.clipsToBounds = true
+        confirmButton.layer.cornerRadius = 12
+    }
+    
+    @IBAction func clickConfirmButton(_ sender: UIButton) {
+        
+    }
+}
