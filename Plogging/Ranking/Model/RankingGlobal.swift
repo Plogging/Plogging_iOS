c97b69cc124cb3526f29cf8ce7627ac12bb5bdea
김혜리 <hyer1k@naver.com>
2021-02-05 03:29:27 +0900

    create Ranking models

---
+++ Plogging/Ranking/Model/RankingGlobal.swift
@@ -0,0 +1,8 @@
+//
+//  RankingGlobal.swift
+//  Plogging
+//
+//  Created by 김혜리 on 2021/02/05.
+//
+
+import Foundation
---
+++ Plogging/Ranking/Model/RankingUser.swift
@@ -0,0 +1,8 @@
+//
+//  RankingUser.swift
+//  Plogging
+//
+//  Created by 김혜리 on 2021/02/05.
+//
+
+import Foundation
