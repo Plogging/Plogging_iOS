77ed2eda08a60bfdffd4b3caf1a1fdbc26c8c4a2
김혜리 <hyer1k@naver.com>
2021-02-13 02:51:44 +0900

    비밀번호 찾기 validation check

--- Plogging/SignInSignUp/Controller/FindPasswordViewController.swift
+++ Plogging/SignInSignUp/Controller/FindPasswordViewController.swift
@@ -12,7 +12,8 @@ class FindPasswordViewController: UIViewController {
     @IBOutlet var emailView: UIView!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var confirmButton: UIButton!
-    
+    @IBOutlet weak var errorLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,6 +32,15 @@ class FindPasswordViewController: UIViewController {
         emailView.layer.cornerRadius = 4
     }
     
+    private func setupWarningLabel(message: String?) {
+        if message != nil {
+            errorLabel.isHidden = false
+            errorLabel.text = message
+        } else {
+            errorLabel.isHidden = true
+        }
+    }
+    
     @IBAction func clickConfirmButton(_ sender: UIButton) {
         guard let email = emailTextField.text else {
             return
@@ -40,11 +50,36 @@ class FindPasswordViewController: UIViewController {
         let param: [String: Any] = ["email": email]
         
         APICollection.sharedAPI.requestUserPasswordTemp(param: param) { (response) in
-            let result = try? response.get()
-            print(result)
+            if let code = try? response.get().rc {
+                switch code {
+                case 200:
+                    self.performSegue(withIdentifier: SegueIdentifier.passwordCompletionViewController, sender: nil)
+                    return
+                case 404:
+                    self.setupWarningLabel(message: "유효하지 않은 이메일입니다.")
+                    return
+                default:
+                    print("error")
+                    return
+                }
+            }
         }
-        
-        // success
-        self.performSegue(withIdentifier: SegueIdentifier.passwordCompletionViewController, sender: nil)
     }
 }
+
+extension FindPasswordViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // 이메일 체크
+        if let email = emailTextField.text {
+            if let message = checkEmailVaidation(email: email) {
+                setupWarningLabel(message: message)
+                confirmButton.backgroundColor = UIColor.loginGray
+                return
+            }
+            setupWarningLabel(message: nil)
+            confirmButton.backgroundColor = UIColor.tintGreen
+        }
+    }
+}
+
+extension FindPasswordViewController: LoginValidation {}
