a389b5d874b7e56986d9deff96ac8de4a9edd3d1
김혜리 <hyer1k@naver.com>
2021-02-13 17:45:50 +0900

    주석 추가

--- Plogging/Common/Networking/Cookie.swift
+++ Plogging/Common/Networking/Cookie.swift
@@ -9,20 +9,25 @@ import Foundation
 
 class PloggingCookie {
     static let shared = PloggingCookie()
+    
     let cookie = "cookie"
     
+    /// 처음인지 확인하는 함수
     func isFirstTimeUser() -> Bool {
         return getUserCookie() == nil 
     }
     
+    /// 유저 쿠키 받기
     func getUserCookie() -> String? {
         return UserDefaults.standard.string(forKey: cookie)
     }
     
+    /// 유저 쿠키 저장
     func setUserCookie(cookie: String) {
         UserDefaults.standard.setValue(cookie, forKey: cookie)
     }
     
+    /// 유저 쿠키 삭제
     func removeUserCookie() {
         UserDefaults.standard.removeObject(forKey: cookie)
     }
