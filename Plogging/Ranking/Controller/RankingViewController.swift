e7611929d47fae1d658c0eb58aed559c742eb3a9
김혜리 <hyer1k@naver.com>
2021-02-14 04:12:09 +0900

    set navigation bar clear

--- Plogging/Ranking/Controller/RankingViewController.swift
+++ Plogging/Ranking/Controller/RankingViewController.swift
@@ -37,6 +37,7 @@ class RankingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setNavigationBarClear()
         setupTableView()
         setupRankingTitle()
         createRefreshControl()
