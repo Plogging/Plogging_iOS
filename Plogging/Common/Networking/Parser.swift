bc8255745543338a434f82c5a1795cb83b1bbaad
jeon-soyeong <jsu3417@naver.com>
2021-02-15 03:05:31 +0900

    [#28] parser 프로토콜, PloggingParser 추가

---
+++ Plogging/Common/Networking/Parser.swift
@@ -0,0 +1,31 @@
+//
+//  Parser.swift
+//  Plogging
+//
+//  Created by 전소영 on 2021/02/14.
+//
+
+import Foundation
+
+protocol Parser {
+    func parseList<T>(from data: Data) -> [T]
+    func getEndPageNumber(from data: Data) -> Int
+}
+
+class PloggingParser<T>: Parser {
+    func parseList<T>(from data: Data) -> [T] {
+        guard let value = try? JSONDecoder().decode(PloggingInfo.self, from: data),
+              let loadedContents = value.ploggingList as? [T] else {
+            return []
+        }
+        return loadedContents
+    }
+    
+    func getEndPageNumber(from data: Data) -> Int {
+        guard let value = try? JSONDecoder().decode(PloggingInfo.self, from: data) else {
+            return 0
+        }
+        let endPageNumber = value.meta.endPageNumber
+        return endPageNumber
+    }
+}
