496e7adf80174104374212d826e00ffa03fc333b
jeon-soyeong <jsu3417@naver.com>
2021-02-15 03:20:50 +0900

    [#28] 페이징 테스트 라벨

--- Plogging/Common/Networking/BaseURL.swift
+++ Plogging/Common/Networking/BaseURL.swift
@@ -24,7 +24,7 @@ struct BasePath {
     // PLOGGING
     static let plogging = "/plogging"
     static let ploggingScore = "/plogging/score"
-    static let ploggingResult = "/plogging/jsu3417@naver.com:custom"
+    static let ploggingResult = "/plogging/jsu3417@naver.com:custom" //userId 필요
 
     // RANKING
     static let rankingGlobal = "/rank/global"
