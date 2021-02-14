fc27da9b0c2a53ca73b374876e05b65751acdd57
김혜리 <hyer1k@naver.com>
2021-02-15 00:53:33 +0900

    랭킹 디테일 뷰 UI 작업

--- Plogging/Ranking/Controller/RankingDetailViewController.swift
+++ Plogging/Ranking/Controller/RankingDetailViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 class RankingDetailViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
-    
+
+    @IBOutlet weak var headerView: CustomHeaderView!
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupCollectionView()
         registerNib()
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    private func animateHeader() {
+        self.headerHeightConstraint.constant = 300
+        UIView.animate(withDuration: 0.4,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.7,
+                       initialSpringVelocity: 0.5,
+                       options: .curveEaseInOut,
+                       animations: {
+                        self.view.layoutIfNeeded()
+                       }, completion: nil)
+    }
     
     private func setupCollectionView() {
         collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     private func registerNib() {
@@ -27,6 +53,40 @@ class RankingDetailViewController: UIViewController {
     }
 
 }
+
+extension RankingDetailViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // 헤더 뷰 증가
+        if scrollView.contentOffset.y < 0 {
+            self.headerHeightConstraint.constant += abs(scrollView.contentOffset.y)
+            headerView.incrementColorAlpha(offset: self.headerHeightConstraint.constant)
+            headerView.incrementAlpha(offset: self.headerHeightConstraint.constant)
+        }
+        // 헤더 뷰 감소
+        else if scrollView.contentOffset.y > 0 && self.headerHeightConstraint.constant >= 65 {
+            self.headerHeightConstraint.constant -= scrollView.contentOffset.y/100
+            headerView.decrementColorAlpha(offset: scrollView.contentOffset.y)
+            headerView.decrementAlpha(offset: self.headerHeightConstraint.constant)
+            
+            if self.headerHeightConstraint.constant < 65 {
+                self.headerHeightConstraint.constant = 65
+            }
+        }
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if self.headerHeightConstraint.constant > 300 {
+            animateHeader()
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        if self.headerHeightConstraint.constant > 300 {
+            animateHeader()
+        }
+    }
+}
+
 extension RankingDetailViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 20
@@ -40,4 +100,13 @@ extension RankingDetailViewController: UICollectionViewDataSource {
 
 extension RankingDetailViewController: UICollectionViewDelegate {
     
+}
+
+extension RankingDetailViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let flowayout = collectionViewLayout as? UICollectionViewFlowLayout
+//        let space: CGFloat = (flowayout?.minimumInteritemSpacing ?? 0.0) + (flowayout?.sectionInset.left ?? 0.0) + (flowayout?.sectionInset.right ?? 0.0)
+        let size:CGFloat = (collectionView.frame.size.width - 40) / 2.0
+        return CGSize(width: size, height: size)
+    }
 }
--- Plogging/Ranking/View/RankingDetailCustomHeaderView.swift
+++ Plogging/Ranking/View/RankingDetailCustomHeaderView.swift
@@ -5,4 +5,114 @@
 //  Created by 김혜리 on 2021/02/14.
 //
 
-import Foundation
+import UIKit
+
+class CustomHeaderView: UIView {
+
+    var imageView:UIImageView!
+    var userIcon:UIImageView!
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        setUpView()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUpView()
+    }
+    
+    func setUpUserData() {
+        
+    }
+    
+    func setUpView() {
+        self.backgroundColor = .clear
+        // 배경 이미지 설정
+        imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(imageView)
+        
+        let constraints:[NSLayoutConstraint] = [
+            imageView.topAnchor.constraint(equalTo: self.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ]
+        NSLayoutConstraint.activate(constraints)
+        
+        imageView.image = UIImage(named: "header")
+        imageView.contentMode = .scaleAspectFill
+
+        // 뒤로가기 버튼
+        let backButton = UIButton()
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(backButton)
+        let backButtonConstraints:[NSLayoutConstraint] = [
+            backButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 21),
+            backButton.topAnchor.constraint(equalTo: topAnchor, constant: 63),
+        ]
+        NSLayoutConstraint.activate(backButtonConstraints)
+        backButton.setImage(UIImage(named: "buttonBack"), for: .normal)
+        
+        // 유저 닉네임
+        let nickNameLabel = UILabel()
+        nickNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(nickNameLabel)
+        
+        let nickNameConstraints:[NSLayoutConstraint] = [
+            nickNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25),
+            nickNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 109),
+        ]
+        NSLayoutConstraint.activate(nickNameConstraints)
+        nickNameLabel.text = "나는혜리💙"
+        nickNameLabel.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 35)
+        nickNameLabel.textColor = .white
+        nickNameLabel.textAlignment = .left
+        
+        // 유저 아이콘
+        userIcon = UIImageView()
+        userIcon.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(userIcon)
+        
+        let imageConstraints:[NSLayoutConstraint] = [
+            userIcon.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            userIcon.topAnchor.constraint(equalTo: topAnchor, constant: 78),
+            userIcon.widthAnchor.constraint(equalToConstant: 74),
+            userIcon.heightAnchor.constraint(equalToConstant: 74)
+        ]
+        NSLayoutConstraint.activate(imageConstraints)
+        userIcon.image = UIImage(named: "ranking")
+    }
+    
+    func createStackView() {
+        
+    }
+    
+    func decrementColorAlpha(offset: CGFloat) {
+//        if self.colorView.alpha <= 1 {
+//            let alphaOffset = (offset/500)/85
+//            self.colorView.alpha += alphaOffset
+//        }
+    }
+    
+    func decrementAlpha(offset: CGFloat) {
+        if self.userIcon.alpha >= 0 {
+            let alphaOffset = max((offset - 65)/85.0, 0)
+            self.userIcon.alpha = alphaOffset
+        }
+    }
+    
+    func incrementColorAlpha(offset: CGFloat) {
+//        if self.colorView.alpha >= 0.6 {
+//            let alphaOffset = (offset/200)/85
+//            self.colorView.alpha -= alphaOffset
+//        }
+    }
+    
+    func incrementAlpha(offset: CGFloat) {
+        if self.userIcon.alpha <= 1 {
+            let alphaOffset = max((offset - 65)/85, 0)
+            self.userIcon.alpha = alphaOffset
+        }
+    }
+}
