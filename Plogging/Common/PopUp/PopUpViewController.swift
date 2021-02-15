d6f9dad16632273515c83b9a06616d07c875ec30
김혜리 <hyer1k@naver.com>
2021-02-16 04:52:07 +0900

    로그아웃 시 쿠키 지우기

--- Plogging/Common/PopUp/PopUpViewController.swift
+++ Plogging/Common/PopUp/PopUpViewController.swift
@@ -74,6 +74,7 @@ class PopUpViewController: UIViewController {
                 if let result = try? response.get() {
                     if result.rc == 200 {
                         // 로그인 화면으로 이동
+                        PloggingCookie.shared.removeUserCookie()
                         PloggingUserData.shared.removeUserData()
                         self.makeLoginRootViewController()
                     } else {
