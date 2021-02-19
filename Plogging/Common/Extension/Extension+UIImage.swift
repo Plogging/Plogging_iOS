0c61b13e23bf2a9031f8579e7585c2685945a226
김혜리 <hyer1k@naver.com>
2021-02-19 20:38:22 +0900

    extension 코드 이동

--- Plogging/Common/Extension/Extension+UIImage.swift
+++ Plogging/Common/Extension/Extension+UIImage.swift
@@ -13,4 +13,16 @@ extension UIImage {
             self.draw(in: CGRect(origin: .zero, size: targetSize))
         }
     }
+    
+    enum JPEGQuality: CGFloat {
+        case lowest  = 0
+        case low     = 0.25
+        case medium  = 0.5
+        case high    = 0.75
+        case highest = 1
+    }
+
+    func jpeg(_ jpegQuality: JPEGQuality) -> Data? {
+        return jpegData(compressionQuality: jpegQuality.rawValue)
+    }
 }
--- Plogging/My Page/Controller/SettingViewController.swift
+++ Plogging/My Page/Controller/SettingViewController.swift
@@ -164,17 +164,3 @@ extension SettingViewController: UIImagePickerControllerDelegate, UINavigationCo
         dismiss(animated: true, completion: nil)
     }
 }
-
-extension UIImage {
-    enum JPEGQuality: CGFloat {
-        case lowest  = 0
-        case low     = 0.25
-        case medium  = 0.5
-        case high    = 0.75
-        case highest = 1
-    }
-
-    func jpeg(_ jpegQuality: JPEGQuality) -> Data? {
-        return jpegData(compressionQuality: jpegQuality.rawValue)
-    }
-}
